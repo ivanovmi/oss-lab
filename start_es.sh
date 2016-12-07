2fbbdd50e1301d86ff66be78b6f79f8df5223f54
Boris Pavlovic <boris@pavlovic.me>
2016-12-07 13:48:01 +0000

    Various fixes

--- start_es.sh
+++ start_es.sh
@@ -1,3 +1,3 @@
 echo "Start Elastic"
 mkdir `pwd`/esdata
-docker run -d -v `pwd`/esdata:/usr/share/elasticsearch/data elasticsearch
+docker run --name elastic -d -v `pwd`/esdata:/usr/share/elasticsearch/data -p 9200:9200 elasticsearch
