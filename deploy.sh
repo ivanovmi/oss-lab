1ed5ee0349232e7848d2ffd30a3d18117a546955
Ilya Kharin <akscram@gmail.com>
2016-12-08 17:00:37 +0400

    Move all configs in one place

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
+#!/bin/bash -e
+
+conf="$(pwd)/configs"
+
 echo "Start all jobs"
-docker run -d --name avail-watcher -v `pwd`/avail:/etc/availability seecloud/availability availability-watcher
-docker run -d --name health-collector -v `pwd`/health:/etc/health -e RUN_HEALTH_JOB=True seecloud/health
+docker run -d --name avail-watcher -v $conf/availability:/etc/availability seecloud/availability availability-watcher
+docker run -d --name health-collector -v $conf/health:/etc/health -e RUN_HEALTH_JOB=True seecloud/health
 
 echo "Start all APIs"
 # Start all APIs
-docker run -d --name avail-api -v `pwd`/avail:/etc/availability -p 5002:5000 seecloud/availability ./entrypoint-api.sh
-docker run -d --name health-api -v `pwd`/health:/etc/health -e RUN_HEALTH_API=True -p 5001:5000 seecloud/health
-docker run -d --name ceagle -v `pwd`/ceagle:/etc/ceagle -p  8080:5000 seecloud/ceagle
+docker run -d --name avail-api -v $conf/availability:/etc/availability -p 5002:5000 seecloud/availability ./entrypoint-api.sh
+docker run -d --name health-api -v $conf/health:/etc/health -e RUN_HEALTH_API=True -p 5001:5000 seecloud/health
+docker run -d --name ceagle -v $conf/ceagle:/etc/ceagle -p  8080:5000 seecloud/ceagle
 
 echo "Start UI"
 docker run --name devops-portal -d --net host seecloud/fuel-devops-portal
